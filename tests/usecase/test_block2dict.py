a7405c5547f0293bac94bcc4a51126e5dd945165
hadisheybani <hadi.sheybani1993@hotmail.com>
2020-05-08 17:28:32 +0430

    block2dict test has been changed

--- tests/usecase/test_block2dict.py
+++ tests/usecase/test_block2dict.py
@@ -9,16 +9,16 @@ class TestBlock2Dict:
 
     def test_GivenABlockWhenCallConvertThenItShouldReturnProperDict(self):
         block = Mock(return_value = Block)
-        block.index.return_value = 1
-        block.time_stamp.return_value = 1000
-        block.data.return_value = "hello"
-        block.pre_hash.return_value = "pre_hash"
-        block.block_hash.return_value = "block_hash"
+        block.index = 1
+        block.time_stamp = 1000
+        block.data = "hello"
+        block.pre_hash = "pre_hash"
+        block.block_hash = "block_hash"
         
         block2dict = Block2Dict()
         block_dict = block2dict.convert(block)
-        assert_that(block_dict["index"], "1")
-        assert_that(block_dict["time_stamp"], "1000")
-        assert_that(block_dict["data"], "hello")
-        assert_that(block_dict["pre_hash"], "pre_hash")
-        assert_that(block_dict["block_hash"], "block_hash")
+        assert_that(block_dict["index"], equal_to("1"))
+        assert_that(block_dict["time_stamp"], equal_to("1000"))
+        assert_that(block_dict["data"], equal_to("hello"))
+        assert_that(block_dict["pre_hash"], equal_to("pre_hash"))
+        assert_that(block_dict["block_hash"], equal_to("block_hash"))
